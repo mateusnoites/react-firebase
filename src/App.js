1deea8b4d10f49e43facf7d73ba496d68f0ade6a
Mateus Dias Oliveira <dias.mateus@academico.ifrn.edu.br>
2023-05-19 10:19:00 -0300

    Primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+
+//////////////////////////////////////////         CÓDIGO         ///////////////////////////////////////////////////////////////
+
+function Cabecalho() {
+  return (
+    <div>
+      <h1>Lades</h1>
+      <hr/>
+    </div>
+  );
+}
 
 function App() {
+  const [listaProfessores, setListaProfessores] = useState([]);
+
+  useEffect(() => {
+    const fetchProfessores = async () => {
+      try {
+        const response = await fetch('https://teste-firebase-lades-default-rtdb.firebaseio.com/professores.json');
+        const data = await response.json();
+
+        const professores = [];
+
+        for (const professorId in data) {
+          if (data.hasOwnProperty(professorId)) {
+            const professor = data[professorId];
+            const nomeProfessor = professor.nome;
+            professores.push(<li key ={professorId}>{nomeProfessor}</li>)
+          }
+        }
+
+        setListaProfessores(professores);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchProfessores();
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <Cabecalho />
       </header>
+      <main>
+        <ol>
+          {listaProfessores}
+        </ol>
+      </main>
     </div>
   );
 }
---
+++ src/public/script.js
@@ -0,0 +1,3 @@
+const texto = "Olá, mundo!";
+
+export default texto;
